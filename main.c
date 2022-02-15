c4bd62ae2bc39d4291c9b3e3d843b96042ca9358
VT211DmitriP <dimnori@gmail.com>
2022-02-15 18:01:36 +0300

    task 2

--- main.c
+++ main.c
@@ -10,19 +10,37 @@ int getSum(int *a , int n ) {
 
 //__________ task 1 __________\\
 
-void task1(matrix m) {
+void swapRowsWithMaxAndMinElement(matrix m) {
     position minPosition = getMinValuePos(m);
     position maxPosition = getMaxValuePos(m);
 
     swapRows(m, minPosition.rowIndex, maxPosition.rowIndex);
 }
 
+//__________ task 2 __________\\
+
+int getMax(int *a, int n) {
+    int max = a[0];
+    for (int i = 1; i < n; i++) {
+        if (a[i] > max)
+            max = a[i];
+    }
+    return max;
+}
+
+void sortRowsByMinElement(matrix m) {
+    insertionSortRowsMatrixByRowCriteria(m, getMax);
+}
+
+//__________ task 3 __________\\
+
+
 
 int main() {
     matrix m = getMemMatrix( 3, 3);
     inputMatrix(m);
 
-    task1(m);
+    sortRowsByMinElement(m);
 
     outputMatrix(m);
 
