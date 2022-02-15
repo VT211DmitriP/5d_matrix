292a8c6f7802ddb038f84f0d55031c5646076c42
VT211DmitriP <dimnori@gmail.com>
2022-02-15 18:41:04 +0300

    task 3

--- main.c
+++ main.c
@@ -17,6 +17,57 @@ void swapRowsWithMaxAndMinElement(matrix m) {
     swapRows(m, minPosition.rowIndex, maxPosition.rowIndex);
 }
 
+void test_swapRowsWithMaxAndMinElement_oneElement() {
+    matrix m1 = createMatrixFromArray(
+            (int[]) {
+                    1
+            },
+            1, 1);
+
+    swapRowsWithMaxAndMinElement(m1);
+
+    matrix m2 = createMatrixFromArray(
+            (int[]) {
+                    1
+            },
+            1, 1);
+
+    assert(areTwoMatricesEqual(m1, m2));
+
+    freeMemMatrix(&m1);
+    freeMemMatrix(&m2);
+}
+
+void test_swapRowsWithMaxAndMinElement_maxAndMinInSameRow() {
+    matrix m1 = createMatrixFromArray(
+            (int[]) {
+                    0, 12, 100,
+                    1, 45, 21,
+                    23, 78, 89
+            },
+            3, 3);
+
+    swapRowsWithMaxAndMinElement(m1);
+
+    matrix m2 = createMatrixFromArray(
+            (int[]) {
+                    0, 12, 100,
+                    1, 45, 21,
+                    23, 78, 89
+            },
+            3, 3);
+
+    assert(areTwoMatricesEqual(m1, m2));
+
+    freeMemMatrix(&m1);
+    freeMemMatrix(&m2);
+}
+
+void test_swapRowsWithMaxAndMinElement() {
+    test_swapRowsWithMaxAndMinElement_oneElement();
+    test_swapRowsWithMaxAndMinElement_maxAndMinInSameRow();
+}
+
 //__________ task 2 __________\\
 
 int getMax(int *a, int n) {
@@ -32,17 +83,95 @@ void sortRowsByMinElement(matrix m) {
     insertionSortRowsMatrixByRowCriteria(m, getMax);
 }
 
+void test_sortRowsByMinElement_threeMaxElements() {
+    matrix m1 = createMatrixFromArray(
+            (int[]) {
+                    7, 1, 2,
+                    1, 8, 1,
+                    3, 2, 3
+            },
+            3, 3);
+
+    sortRowsByMinElement(m1);
+
+    matrix m2 = createMatrixFromArray(
+            (int[]) {
+                    3, 2, 3,
+                    7, 1, 2,
+                    1, 8, 1
+            },
+            3, 3);
+
+    assert(areTwoMatricesEqual(m1, m2));
+
+    freeMemMatrix(&m1);
+    freeMemMatrix(&m2);
+}
+
+void test_sortRowsByMinElement() {
+    test_sortRowsByMinElement_threeMaxElements();
+}
+
 //__________ task 3 __________\\
 
+int getMin(int *a, int n) {
+    int min = a[0];
+    for (int i = 1; i < n; i++) {
+        if (a[i] < min)
+            min = a[i];
+    }
+    return min;
+}
 
+void sortColsByMinElement(matrix m) {
+    insertionSortColsMatrixByColCriteria(m, getMin);
+}
+
+void test_sortColsByMinElement_sixMinElements() {
+    matrix m1 = createMatrixFromArray(
+            (int[]) {
+                    3, 5, 2, 4, 3, 3,
+                    2, 5, 1, 8, 2, 7,
+                    6, 1, 4, 4, 8, 3
+            },
+            3, 6);
+
+    sortColsByMinElement(m1);
+
+    matrix m2 = createMatrixFromArray(
+            (int[]) {
+                    5, 2, 3, 3, 3, 4,
+                    5, 1, 2, 2, 7, 8,
+                    1, 4, 6, 8, 3, 4
+            },
+            3, 6);
+
+    assert(areTwoMatricesEqual(m1, m2));
+
+    freeMemMatrix(&m1);
+    freeMemMatrix(&m2);
+}
+
+void test_sortColsByMinElement() {
+    test_sortColsByMinElement_sixMinElements();
+}
+
+
+
+
+
+void test() {
+    test_swapRowsWithMaxAndMinElement();
+    test_sortRowsByMinElement();
+    test_sortColsByMinElement();
+}
 
 int main() {
-    matrix m = getMemMatrix( 3, 3);
-    inputMatrix(m);
+    test();
+    //matrix m = getMemMatrix( 3, 6);
+    //inputMatrix(m);
 
-    sortRowsByMinElement(m);
-
-    outputMatrix(m);
+    //outputMatrix(m);
 
     return 0;
 }
